291f788068ba7cc70d14afb7c91b4d261fe2a523
citywidekeystop-collab <citywidekeystop@gmail.com>
2025-12-21 21:51:22 -0500

    Update SMS log message to use OWNER

--- server.js
+++ server.js
@@ -58,7 +58,7 @@ app.post("/lead/new", async (req, res) => {
         body: msg,
       });
 
-      console.log("📲 SMS sent to OWNER_NUMBER");
+      console.log("📲 SMS sent to OWNER");
     } else {
       console.log("SMS skipped (missing Twilio or OWNER_NUMBER)");
     }
